5650a90d132066f6af5e291ceddc49df821d94f7
witness449 <lebedev_84@mail.ru>
2025-05-07 14:34:46 +0000

    Разработка структуры класса Impl

--- src/main.cpp
+++ src/main.cpp
@@ -7,37 +7,12 @@
 #include <stdexcept>
 #include <string>
 
-struct AesCipherParams {
-    static const size_t KEY_SIZE = 32;             // AES-256 key size
-    static const size_t IV_SIZE = 16;              // AES block size (IV length)
-    const EVP_CIPHER *cipher = EVP_aes_256_cbc();  // Cipher algorithm
-
-    int encrypt;                              // 1 for encryption, 0 for decryption
-    std::array<unsigned char, KEY_SIZE> key;  // Encryption key
-    std::array<unsigned char, IV_SIZE> iv;    // Initialization vector
-};
-
-AesCipherParams CreateChiperParamsFromPassword(std::string_view password) {
-    AesCipherParams params;
-    constexpr std::array<unsigned char, 8> salt = {'1', '2', '3', '4', '5', '6', '7', '8'};
-
-    int result = EVP_BytesToKey(params.cipher, EVP_sha256(), salt.data(),
-                                reinterpret_cast<const unsigned char *>(password.data()), password.size(), 1,
-                                params.key.data(), params.iv.data());
-
-    if (result == 0) {
-        throw std::runtime_error{"Failed to create a key from password"};
-    }
-
-    return params;
-}
-
 int main(int argc, char *argv[]) {
     try {
         //
         // OpenSSL пример использования:
         //
-        std::string input = "01234567890123456789";
+        /*std::string input = "01234567890123456789";
         std::string output;
 
         OpenSSL_add_all_algorithms();
@@ -76,6 +51,7 @@ int main(int argc, char *argv[]) {
         //
         // Конец примера
         //
+        */
 
         CryptoGuard::ProgramOptions options;
 
