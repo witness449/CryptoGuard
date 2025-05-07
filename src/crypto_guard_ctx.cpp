7c832fe83978b94545b0211458d124d3a18cb50d
witness449 <lebedev_84@mail.ru>
2025-05-07 14:35:13 +0000

    Clang format поправил

--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -4,53 +4,50 @@
 
 namespace CryptoGuard {
 
-//Структура для хранения параметров шифрования    
+// Структура для хранения параметров шифрования
 struct AesCipherParams {
     static const size_t KEY_SIZE = 32;             // AES-256 key size
     static const size_t IV_SIZE = 16;              // AES block size (IV length)
     const EVP_CIPHER *cipher = EVP_aes_256_cbc();  // Cipher algorithm
-    
+
     int encrypt;                              // 1 for encryption, 0 for decryption
     std::array<unsigned char, KEY_SIZE> key;  // Encryption key
     std::array<unsigned char, IV_SIZE> iv;    // Initialization vector
 };
-    
-struct CryptoGuardCtx::Impl {
-    
-    //Указатель на CTX структуру
-    EVP_CIPHER_CTX* ctx;
 
-    //Констурктор в котором осуществляется захват ресурса
-    Impl(){
+struct CryptoGuardCtx::Impl {
+
+    // Указатель на CTX структуру
+    EVP_CIPHER_CTX *ctx;
+
+    // Констурктор в котором осуществляется захват ресурса
+    Impl() {
         OpenSSL_add_all_algorithms();
         auto *ctx = EVP_CIPHER_CTX_new();
     }
 
-    //В деструкторе освобождаем ресурс
-    ~Impl(){
-        EVP_CIPHER_CTX_free(ctx);
-    }
-    
+    // В деструкторе освобождаем ресурс
+    ~Impl() { EVP_CIPHER_CTX_free(ctx); }
+
     AesCipherParams CreateChiperParamsFromPassword(std::string_view password) {
         AesCipherParams params;
         constexpr std::array<unsigned char, 8> salt = {'1', '2', '3', '4', '5', '6', '7', '8'};
-        
+
         int result = EVP_BytesToKey(params.cipher, EVP_sha256(), salt.data(),
                                     reinterpret_cast<const unsigned char *>(password.data()), password.size(), 1,
                                     params.key.data(), params.iv.data());
-        
+
         if (result == 0) {
             throw std::runtime_error{"Failed to create a key from password"};
         }
-        
+
         return params;
     }
 
-    //Заготовка приватных методов шифрования, дешифрования и расчета контрольной суммы
+    // Заготовка приватных методов шифрования, дешифрования и расчета контрольной суммы
     void Encrypt(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
     void Decrypt(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
     std::string CalculateChecksum(std::iostream &inStream) { return "NOT IMPLEMENTED"; }
-    
 };
 
 CryptoGuardCtx::CryptoGuardCtx() : pImpl_(std::make_unique<Impl>()){};
